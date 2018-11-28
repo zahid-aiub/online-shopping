9a28ab5df31d9c0fe1cc0c9819a5c739ee80d185
Zahid-shr <zahid.aiub6@gmail.com>
2018-11-29 00:24:03 +0600

    My project setup completed successfully.

---
+++ shoppingBackend/src/main/java/net/zahid/shoppingBackend/App.java
@@ -0,0 +1,13 @@
+package net.zahid.shoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
